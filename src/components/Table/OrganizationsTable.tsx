47d96628e1415dae9e411edf11ac33f0ef29a7c8
bsiman2 <b.siman@vegait.rs>
2022-01-04 10:32:48 +0100

    added sorting to organizations page

--- src/components/Table/OrganizationsTable.tsx
+++ src/components/Table/OrganizationsTable.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Table } from "react-bootstrap";
 import { OrganizationModel } from "../../features/organization/providers/types";
 
@@ -6,55 +6,90 @@ interface Props {
   head: string[];
   rows: OrganizationModel[];
   clickHandler: (id: string) => void;
+  sortHandler: (field: string, direction: boolean) => void;
 }
 
-const OrganizationTable = ({ head, rows, clickHandler }: Props) => {
-  
+const OrganizationTable = ({ head, rows, clickHandler, sortHandler }: Props) => {
+  const [sortDirection, setSortDirection] = useState(false);
+  const [activeSortField, setActiveSortField] = useState("");
+
   const getChildren = (children: OrganizationModel[], ident: number): any => {
     if (children !== undefined && children.length > 0) {
       return children.map((row) => {
-        return [<tr key={row.identifier} onClick={() => {
-          clickHandler(row.identifier);
-        }}>
-            <td><span style={{paddingLeft:ident.toString() + "px"}}>{row.name}</span></td>
+        return [
+          <tr
+            key={row.identifier}
+            onClick={() => {
+              clickHandler(row.identifier);
+            }}
+          >
+            <td>
+              <span style={{ paddingLeft: ident.toString() + "px" }}>
+                {row.name}
+              </span>
+            </td>
             <td>{row.type.valueCodableConcept}</td>
             <td>{row.active.toString()}</td>
-          </tr>, ...getChildren(row.headOf, ident + 20)]
-        });
-      } else {
-        return []
-      }
+          </tr>,
+          ...getChildren(row.headOf, ident + 20),
+        ];
+      });
+    } else {
+      return [];
     }
+  };
 
   return (
     <>
-    <Table bordered responsive>
-      <thead className="border border-2">
-        <tr>
-          {head.map((el, index) => {
-            return <th key={index}>{el}</th>;
+      <Table bordered responsive>
+        <thead className="border border-2">
+          <tr>
+            {head.map((el, index) => {
+              return (
+                <th
+                  key={index}
+                  onClick={() => {
+                    setSortDirection(!sortDirection);
+                    setActiveSortField(el);
+                    sortHandler(el, sortDirection);
+                  }}
+                >
+                  {el}
+                  {activeSortField === el
+                    ? sortDirection
+                      ? " ▲"
+                      : " ▼"
+                    : null}
+                </th>
+              );
+            })}
+          </tr>
+        </thead>
+        <tbody>
+          {rows.map((row) => {
+            return [
+              <tr
+                key={row.identifier}
+                onClick={() => {
+                  clickHandler(row.identifier);
+                }}
+              >
+                <td>
+                  <span style={{ paddingLeft: "10px", fontWeight: "bold" }}>
+                    {row.name}
+                  </span>
+                </td>
+                <td>{row.type.valueCodableConcept}</td>
+                <td>{row.active.toString()}</td>
+              </tr>,
+              ...getChildren(row.headOf, 20),
+            ];
           })}
-        </tr>
-      </thead>
-      <tbody>
-        {rows.map((row) => {
-          return [
-            <tr
-              key={row.identifier}
-              onClick={() => {
-                clickHandler(row.identifier);
-              }}
-            >
-              <td><span style={{paddingLeft: "10px", fontWeight: 'bold'}}>{row.name}</span></td>
-              <td>{row.type.valueCodableConcept}</td>
-              <td>{row.active.toString()}</td>
-            </tr>,
-            ...getChildren(row.headOf, 20),
-          ];
-        })}
-      </tbody>
-    </Table>
-    {rows.length === 0 && <p className="text-center lead">No organizations found.</p>}
+        </tbody>
+      </Table>
+      {rows.length === 0 && (
+        <p className="text-center lead">No organizations found.</p>
+      )}
     </>
   );
 };
