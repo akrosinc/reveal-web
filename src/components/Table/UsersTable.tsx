654a34a62e29f6e2a68581bab9191749d8509021
bsiman2 <b.siman@vegait.rs>
2022-01-04 11:51:30 +0100

    added sorting to bulk imports, fixed views when there is no data and disabled search inputs in that case.

--- src/components/Table/UsersTable.tsx
+++ src/components/Table/UsersTable.tsx
@@ -13,62 +13,61 @@ const UsersTable = ({ head, rows, clickHandler, sortHandler }: Props) => {
   const [sortDirection, setSortDirection] = useState(false);
   const [activeSortField, setActiveSortField] = useState("");
   return (
-    <>
-      <Table bordered responsive>
-        <thead className="border border-2">
-          <tr>
-            {head.map((el, index) => {
-              return (
-                <th
-                  key={index}
-                  onClick={() => {
-                    setSortDirection(!sortDirection);
-                    setActiveSortField(el);
-                    sortHandler(el, sortDirection);
-                  }}
-                >
-                  {el}
-                  {activeSortField === el ? (
-                    sortDirection ? (
-                      " ▲"
-                    ) : (
-                      " ▼"
-                    )
-                  ) : null}
-                </th>
-              );
-            })}
-          </tr>
-        </thead>
-        <tbody>
-          {rows.length > 0
-            ? rows.map((user, index) => {
+      rows.length === 0 ? (
+        <p className="text-center lead">No users found.</p>
+      ) : (
+        <Table bordered responsive>
+          <thead className="border border-2">
+            <tr>
+              {head.map((el, index) => {
                 return (
-                  <tr
+                  <th
                     key={index}
                     onClick={() => {
-                      if (clickHandler !== undefined) {
-                        clickHandler(user.identifier);
-                      }
+                      setSortDirection(!sortDirection);
+                      setActiveSortField(el);
+                      sortHandler(el, sortDirection);
                     }}
                   >
-                    <td>{user.username}</td>
-                    <td>{user.firstName}</td>
-                    <td>{user.lastName}</td>
-                    <td>
-                      {user.organizations
-                        .map((el) => el.name)
-                        .sort()
-                        .toString()}
-                    </td>
-                  </tr>
+                    {el}
+                    {activeSortField === el
+                      ? sortDirection
+                        ? " ▲"
+                        : " ▼"
+                      : null}
+                  </th>
                 );
-              })
-            : null}
-        </tbody>
-      </Table>
-      {rows.length === 0 && <p className="text-center lead">No users found.</p>}
-    </>
+              })}
+            </tr>
+          </thead>
+          <tbody>
+            {rows.length > 0
+              ? rows.map((user, index) => {
+                  return (
+                    <tr
+                      key={index}
+                      onClick={() => {
+                        if (clickHandler !== undefined) {
+                          clickHandler(user.identifier);
+                        }
+                      }}
+                    >
+                      <td>{user.username}</td>
+                      <td>{user.firstName}</td>
+                      <td>{user.lastName}</td>
+                      <td>
+                        {user.organizations
+                          .map((el) => el.name)
+                          .sort()
+                          .toString()}
+                      </td>
+                    </tr>
+                  );
+                })
+              : null}
+          </tbody>
+        </Table>
+      )
   );
 };
 
