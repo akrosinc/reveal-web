1a6f6cb7075245ae21d9d82e9c6fe3fbcda5461c
bsiman2 <b.siman@vegait.rs>
2022-08-18 10:51:54 +0200

    added search on tags page and plan simulation modal

--- src/features/tagging/components/Tagging.tsx
+++ src/features/tagging/components/Tagging.tsx
@@ -1,5 +1,6 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, Col, Row } from 'react-bootstrap';
+import { DebounceInput } from 'react-debounce-input';
 import { toast } from 'react-toastify';
 import { PageableModel } from '../../../api/providers';
 import Paginator from '../../../components/Pagination';
@@ -9,14 +10,26 @@ import { getAllGlobalTags } from '../api';
 import { Tag } from '../providers/types';
 import CreateTag from './createModal';
 
-const columnsNotForDisplay = ['valueType', 'resultLiteral', 'resultExpression', 'generated', 'addToMetadata', 'fieldType', 'referenceFields', 'generationFormula']
+const columnsNotForDisplay = [
+  'valueType',
+  'resultLiteral',
+  'resultExpression',
+  'generated',
+  'addToMetadata',
+  'fieldType',
+  'referenceFields',
+  'generationFormula'
+];
 
 const Tagging = () => {
   const [tagList, setTagList] = useState<PageableModel<Tag>>();
   const [showCreate, setShowCreate] = useState(false);
+  const [currentSortDirection, setCurrentSortDirection] = useState<boolean>();
+  const [currentSortField, setCurrentSortField] = useState('');
+  const [currentSearchInput, setCurrentSearchInput] = useState('');
 
-  const loadData = useCallback((size: number, page: number, field?: string, direction?: boolean) => {
-    getAllGlobalTags(size, page, field, direction)
+  const loadData = useCallback((size: number, page: number, filter?: string, field?: string, direction?: boolean) => {
+    getAllGlobalTags(size, page, filter, field, direction)
       .then(res => {
         res.content.forEach(el => {
           el.lookupEntityType = el.lookupEntityType.code as any;
@@ -31,31 +44,54 @@ const Tagging = () => {
   }, [loadData]);
 
   const paginationHandler = (size: number, page: number) => {
-    loadData(size, page);
+    loadData(size, page, currentSearchInput, currentSortField, currentSortDirection);
   };
 
   const sortHandler = (field: string, direction: boolean) => {
-    loadData(PAGINATION_DEFAULT_SIZE, 0, field, direction);
+    setCurrentSortDirection(direction);
+    setCurrentSortField(field);
+    loadData(PAGINATION_DEFAULT_SIZE, 0, currentSearchInput, field, direction);
+  };
+
+  const filterData = (e: any) => {
+    setCurrentSearchInput(e.target.value);
+    loadData(tagList?.size ?? PAGINATION_DEFAULT_SIZE, 0, e.target.value);
   };
 
   return (
     <>
-      <div className="d-flex justify-content-between">
-        <h2>Tags({tagList?.totalElements})</h2>
-        <Button onClick={() => setShowCreate(true)}>Create Tag</Button>
-      </div>
+      
+        <h2>Tags({tagList?.totalElements})        
+        <Row className="my-4">
+          <Col md={8} className="mb-2">
+          <Button className='float-end' onClick={() => setShowCreate(true)}>Create Tag</Button>
+          </Col>
+          <Col sm={12} md={4} className="order-md-first">
+            <DebounceInput
+              id="search-tags"
+              className="form-control"
+              placeholder="Search Tags"
+              debounceTimeout={800}
+              onChange={e => filterData(e)}
+              disabled={tagList?.totalElements === 0}
+            />
+          </Col>
+        </Row>
+        </h2>
       <hr />
-      {tagList && tagList.content.length && (
+      {tagList && tagList.content.length > 0 ? (
         <>
           <DefaultTable
             sortHandler={sortHandler}
-            columns={Object.keys(tagList.content[0]).filter(el => !columnsNotForDisplay.includes(el)).map(el => {
-              return {
-                name: el.charAt(0).toUpperCase() + el.slice(1),
-                accessor: el,
-                sortValue: el
-              };
-            })}
+            columns={Object.keys(tagList.content[0])
+              .filter(el => !columnsNotForDisplay.includes(el))
+              .map(el => {
+                return {
+                  name: el.charAt(0).toUpperCase() + el.slice(1),
+                  accessor: el,
+                  sortValue: el
+                };
+              })}
             data={tagList?.content}
           />
           <Paginator
@@ -66,6 +102,8 @@ const Tagging = () => {
             paginationHandler={paginationHandler}
           />
         </>
+      ) : (
+        <p>No data found.</p>
       )}
       {showCreate && (
         <CreateTag
