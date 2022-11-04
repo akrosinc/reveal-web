9282ec3b9d73074f74c32b369a3c18cf30e16a12
trevlenb2 <trevlenb2@gmail.com>
2022-11-04 14:28:33 +0200

    test

--- src/features/plan/components/create/Goals/Actions/Actions.tsx
+++ src/features/plan/components/create/Goals/Actions/Actions.tsx
@@ -18,7 +18,7 @@ interface Props {
 }
 
 const Actions = ({ closeHandler, planPeriod, selectedAction }: Props) => {
-  const [formList, setFormList] = useState<{ identifier: string; name: string }[]>();
+  // const [formList, setFormList] = useState<{ identifier: string; name: string }[]>();
   const [actionTitles, setActionTitles] = useState<string[]>([]);
   const isDarkMode = useAppSelector(state => state.darkMode.value);
   
@@ -53,7 +53,7 @@ const Actions = ({ closeHandler, planPeriod, selectedAction }: Props) => {
 
   useEffect(() => {
     getformList().then(res => {
-      setFormList(res);
+      // setFormList(res);
       setValue('formIdentifier', selectedAction?.formIdentifier ?? '');
     });
   }, [setValue, selectedAction]);
