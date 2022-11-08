c00ac2e6ce25ad084a4ddc10b6795ed1713a81a9
Linda Oscar Gcaba <lindagcaba@gmail.com>
2022-11-08 18:03:52 +0200

    cleanup

--- src/features/reporting/components/report/Report.tsx
+++ src/features/reporting/components/report/Report.tsx
@@ -351,7 +351,7 @@ const Report = () => {
         });
       }
     },
-    [planId, reportType, loadData, selectedMdaLiteReport]
+    [planId, reportType, loadData]
   );
 
   const breadCrumbClickHandler = (el: BreadcrumbModel, index: number) => {
