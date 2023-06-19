3c2d1260118750fda7b943354f607c0369f8a389
trevlenb2 <trevlenb2@gmail.com>
2023-06-19 15:56:13 +0200

    simulation enhancements and mali

--- src/pages/Management/Management.tsx
+++ src/pages/Management/Management.tsx
@@ -1,32 +1,25 @@
-import { useEffect } from 'react';
 import { Tab, Tabs } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import AuthGuard from '../../components/AuthGuard';
 import PageWrapper from '../../components/PageWrapper';
 import { MANAGEMENT, ORGANIZATION_VIEW, ROLE_MANAGE_USER } from '../../constants';
 import Organization from '../../features/organization/components';
 import UserImport from '../../features/user/components/UserImport/UserImport';
 import Users from '../../features/user/components/UsersPage';
+import { useKeycloak } from '@react-keycloak/web';
 
 const Management = () => {
   const { t } = useTranslation();
 
-  let { tab } = useParams();
   let navigate = useNavigate();
 
-  useEffect(() => {
-    if (tab === undefined) {
-      navigate(MANAGEMENT + '/user');
-    } else if (tab !== 'organization' && tab !== 'user' && tab !== 'user-import') {
-      navigate('/error');
-    }
-  }, [tab, navigate]);
+  const { keycloak } = useKeycloak();
 
   return (
     <PageWrapper>
       <Tabs
-        defaultActiveKey={tab}
+        defaultActiveKey={'user'}
         id="management-tab"
         className="mb-3"
         mountOnEnter={true}
@@ -35,11 +28,14 @@ const Management = () => {
           navigate(MANAGEMENT + '/' + tabName);
         }}
       >
-        <Tab eventKey="organization" title={t('managementPage.organization')}>
-          <AuthGuard roles={[ORGANIZATION_VIEW]}>
-            <Organization />
-          </AuthGuard>
-        </Tab>
+        {keycloak.hasRealmRole(ORGANIZATION_VIEW) && (
+          <Tab eventKey="organization" title={t('managementPage.organization')}>
+            <AuthGuard roles={[ORGANIZATION_VIEW]}>
+              <Organization />
+            </AuthGuard>
+          </Tab>
+        )}
+
         <Tab eventKey="user" title={t('managementPage.user')}>
           <AuthGuard roles={[ROLE_MANAGE_USER]}>
             <Users />
