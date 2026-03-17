c7758298ddb0de48a5cc6ada3e8f555083c9e5d5
tatheeratjugrafiya <tatheer.jugrafiya@gmail.com>
2026-03-17 10:15:34 +0500

    Accordion removed (Group management)

--- src/features/groupConfiguration/components/AreasSelection.tsx
+++ src/features/groupConfiguration/components/AreasSelection.tsx
@@ -496,18 +496,18 @@ const AreasSelection: React.FC<Props> = ({
           Areas
         </Card.Header>
         <Card.Body className="p-3" style={{ background: isDarkMode ? '#282828' : '#fff',height:266,overflowY:'auto' }}>
-          <div className="area-selection-content">
+          <div className="area-selection-content" >
             <div className="mb-3">
               <div className="mb-2">
-                <div
+                {/* <div
                   onClick={() => setHierarchyOpen(!hierarchyOpen)}
                   className="d-flex align-items-center justify-content-between p-3 border-bottom"
                   style={{ cursor: 'pointer', fontSize: '1rem', color: isDarkMode ? '#fff' : '#000' }}
                 >
                    <span className="fw-bold">Areas Hierarchy</span>
                   <FontAwesomeIcon icon={hierarchyOpen ? faChevronDown : faChevronRight} size="xs" className="text-secondary" />
-                </div>
-                <Collapse in={hierarchyOpen}>
+                </div> */}
+                {/* <Collapse in={hierarchyOpen}> */}
                   <div>
                     <div className="p-3 border-bottom">
                       <div className="position-relative">
@@ -559,7 +559,7 @@ const AreasSelection: React.FC<Props> = ({
                       )}
                     </div>
                   </div>
-                </Collapse>
+                {/* </Collapse> */}
               </div>
             </div>
           </div>
