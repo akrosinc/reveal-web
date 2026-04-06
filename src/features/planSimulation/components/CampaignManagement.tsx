2fb4f40cbaa552ad7f4004a249330407f64b4a1f
haiderali22 <58180942+haiderali22@users.noreply.github.com>
2026-04-06 18:35:32 +0500

    fixed bug CampaignManagement.tsx where instance id was sent instead of plan id

--- src/features/planSimulation/components/CampaignManagement.tsx
+++ src/features/planSimulation/components/CampaignManagement.tsx
@@ -586,7 +586,7 @@ const CampaignManagement = () => {
   const fetchPlanInfo = async (selectedPlan: any) => {
     try {
       const planInfo = selectedPlan;
-      dispatch({ type: 'SET_PLANID', payload: planInfo.identifier });
+      dispatch({ type: 'SET_PLANID', payload: planInfo.planIdentifier });
       dispatch({ type: 'SET_PLAN_TARGET_TYPE', payload: planInfo?.planTargetType });
       return planInfo?.planIdentifier || planInfo?.identifier;
     } catch (error) {
