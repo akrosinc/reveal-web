2ee1ccc2913f3c8c33ac44040ca32975fa7486ac
trevlenb2 <trevlenb2@gmail.com>
2026-04-12 21:06:44 +0200

    merge 2026

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-CI=false NODE_OPTIONS="--max-old-space-size=4096" GENERATE_SOURCEMAP=false npm run build
-serve -s build
+#CI=false NODE_OPTIONS="--max-old-space-size=4096" GENERATE_SOURCEMAP=false npm run build
+#serve -s build
+npm run start
